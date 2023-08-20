df24f59406426da5a3b78b740c7d56871b33fcac
Matias E. Carballo <95238399+lokywolf2295@users.noreply.github.com>
2023-08-20 20:36:17 -0300

    Update(Client 1): 3 add content to .js files

---
+++ client/js/products.js
@@ -0,0 +1,30 @@
+const productos = [
+    {
+        id: 1,
+        productName: "Banana",
+        price: 400,
+        quantity: 1,
+        img: "/client/media/banana.jpg",
+    },
+    {
+        id: 2,
+        productName: "Leche",
+        price: 950,
+        quantity: 1,
+        img: "/client/media/leche.jpg",
+    },
+    {
+        id: 3,
+        productName: "Pollo",
+        price: 750,
+        quantity: 1,
+        img: "/client/media/pollo.jpg",
+    },
+    {
+        id: 4,
+        productName: "Mayonesa",
+        price: 510,
+        quantity: 1,
+        img: "/client/media/mayonesa.jpg",
+    },
+];
