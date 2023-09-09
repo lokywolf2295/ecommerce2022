d1ebf8dfc8786bf5748f6bdf562e94d4e1e0e64e
lokywolf2295 <matiasesek1995@gmail.com>
2023-09-08 21:20:42 -0300

    Update(Client 3): 3 add repeat logic in index.js file

--- client/js/index.js
+++ client/js/index.js
@@ -2,21 +2,32 @@ const shopContent = document.getElementById("shopContent");
 const cart = []; //Este es nuestro carrito, un array vacío
 
 productos.forEach((product) => {
-    const content = document.createElement("div");
-    content.className = "card";
-    content.innerHTML = `
+  const content = document.createElement("div");
+  content.className = "card";
+  content.innerHTML = `
     <img src="${product.img}">
     <h3>${product.productName}</h3>
     <p class="price">${product.price} $</p>
     `;
-    shopContent.append(content);
+  shopContent.append(content);
 
-    const buyButton = document.createElement("button");
-    buyButton.innerText = "Buy";
+  const buyButton = document.createElement("button");
+  buyButton.innerText = "Buy";
 
-    content.append(buyButton);
+  content.append(buyButton);
 
-    buyButton.addEventListener("click", () => {
+  buyButton.addEventListener("click", () => {
+    const repeat = cart.some(
+      (repeatProduct) => repeatProduct.id === product.id
+    );
+
+    if (repeat) {
+        cart.map((prod) => {
+            if (prod.id === product.id) {
+            prod.quanty += 1;
+            }
+        });
+    } else {
         cart.push({
             id: product.id,
             productName: product.productName,
@@ -24,6 +35,6 @@ productos.forEach((product) => {
             quanty: product.quanty,
             img: product.img,
         });
-        console.log(cart);
-    });
+    }
+  });
 });
